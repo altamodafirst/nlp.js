cb712113b1ada589c853ac68f01d6d91b86bb6e7
Amber Nifong <amber.nifong@gmail.com>
2022-03-13 19:41:37 -0400

    debugging

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const { dockStart } = require('@nlpjs/basic');
-const PORT = process.env.PORT || 3000
-const app = require('./app')
-// express
 
 (async () => {
   await dockStart();
-  app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
 })();
