34e3f03084ab212f20e1831c804dba84e3205fa5
Amber Nifong <amber.nifong@gmail.com>
2022-03-13 19:32:08 -0400

    debugging

---
+++ app.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+module.exports = app
+
+
+// body parsing middleware
+app.use(express.json())
+
+// auth and api routes
+
+app.use('/api', require('./api'))
+
+// static file-serving middleware
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+// any remaining requests with an extension (.js, .css, etc.) send 404
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error('Not found')
+    err.status = 404
+    next(err)
+  } else {
+    next()
+  }
+})
+
+
+// error handling endware
+app.use((err, req, res, next) => {
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const { dockStart } = require('@nlpjs/basic');
-
-
+const PORT = process.env.PORT || 3000
+const app = require('./app')
 // express
 
 (async () => {
   await dockStart();
+  app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
 })();
