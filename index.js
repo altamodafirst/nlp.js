21b13406477dc3d3132c5a6a32220cb23f86d7d8
Amber Nifong <amber.nifong@gmail.com>
2022-03-13 18:13:40 -0400

    bug

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const { dockStart } = require('@nlpjs/basic');
-const cool = require('cool-ascii-faces');
-const express = require('express');
-const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
 
 // express
 
 (async () => {
   await dockStart();
-  app.get('/cool', (req, res) => res.send(cool()));
-  app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 })();
